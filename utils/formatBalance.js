6509ea2ed9d6180ecd35e2740b69202247f4aad5
Valdemar <g1deoniusimo@gmail.com>
2021-09-20 21:36:26 +0300

    fix formatBalance

--- utils/formatBalance.js
+++ utils/formatBalance.js
@@ -1,10 +1,12 @@
+const Big = require('big.js')
 const formatBigNumberStr = require('./formatBigNumberStr')
 
 module.exports = (value) => {
-  if (value.gte(1)) {
-    return formatBigNumberStr(value.toFixed(0, 0))
+  const balance = new Big(value)
+  if (balance.gte(1)) {
+    return formatBigNumberStr(balance.toFixed(0, 0))
   }
-  const str = value.toFixed(9, 0)
+  const str = balance.toFixed(9, 0)
   let index = -1
   for (let i = str.length - 1; i >= 0; i--) {
     if (str[i] !== '0') {
