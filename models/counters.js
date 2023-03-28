31c7d40b23b8e1276f15e0d974a6271365c82966
undefined <44500896+Vandomas@users.noreply.github.com>
2023-03-28 20:36:55 +0300

    update .gitigoner, delete block.js, add  last_checked_block

--- models/counters.js
+++ models/counters.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const countersSchema = new mongoose.Schema(
   {
     send_notifications: Number,
+    last_checked_block: Number
   },
   {
     versionKey: false,
