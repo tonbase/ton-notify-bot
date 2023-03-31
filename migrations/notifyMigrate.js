e3c782a3fdbc92c749fb2a968dd0b567abad7afb
undefined <44500896+Vandomas@users.noreply.github.com>
2023-04-01 00:28:20 +0300

    fix migration script

--- migrations/notifyMigrate.js
+++ migrations/notifyMigrate.js
@@ -6,19 +6,24 @@ const log = require('../utils/log')
 mongoose
   .connect(config.get('db'))
   .then(async () => {
-    await AddressModel.updateMany({}, [
+    await AddressModel.updateMany(
       {
-        $set: {
-          'notifications.is_enabled': {
-            $cond: {
-              if: { $eq: ['$notifications', true] },
-              then: true,
-              else: false,
+        'notifications.is_enabled': { $exists: false },
+      },
+      [
+        {
+          $set: {
+            'notifications.is_enabled': {
+              $cond: {
+                if: { $eq: ['$notifications', true] },
+                then: true,
+                else: false,
+              },
             },
           },
         },
-      },
-    ])
+      ],
+    )
     log.info('Migration completed')
     process.exit(1)
   })
