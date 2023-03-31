56a10ac2b3f3daf924c6047c8135e8a4dfbeae62
undefined <44500896+Vandomas@users.noreply.github.com>
2023-03-31 21:28:28 +0300

    add title to message about editing min amount

--- handlers/handleMinAmount.js
+++ handlers/handleMinAmount.js
@@ -2,19 +2,24 @@ const { Extra } = require('telegraf')
 const AddressRepository = require('../repositories/address')
 const getBackToAddressKeyboard = require('../keyboards/backToAddress')
 const formatAddress = require('../utils/formatAddress')
+const formatTag = require('../utils/formatTag')
 
 module.exports = async (ctx) => {
   const [addressId] = ctx.match
 
   const addressRepository = new AddressRepository()
-  const { _id, address, is_deleted: isDeleted } = await addressRepository.getOneById(addressId)
+  const { _id, address, tag, is_deleted: isDeleted } = await addressRepository.getOneById(addressId)
 
   if (isDeleted) {
     return ctx.answerCbQuery()
   }
 
   await ctx.editMessageText(
-    ctx.i18n.t('address.notifications.edit', { address, formatAddress }),
+    ctx.i18n.t('address.notifications.edit', {
+      address,
+      formatAddress,
+      tag: formatTag(tag),
+    }),
     Extra.HTML().webPreview(false).markup(getBackToAddressKeyboard(_id, ctx.i18n)),
   )
 
