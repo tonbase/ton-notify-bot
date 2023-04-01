d0047149e1c29754b77accbbe4920eb93b965c91
undefined <44500896+Vandomas@users.noreply.github.com>
2023-04-01 19:39:27 +0300

    add scene leave and more code short

--- handlers/editMinAmount.js
+++ handlers/editMinAmount.js
@@ -19,13 +19,14 @@ module.exports = async (ctx) => {
       throw new Error('Value more than 5 000 000 000')
     }
 
-    const rawAmount = ton.utils.toNano(val.toString())
-    const nanoAmount = ton.utils.BN(rawAmount).toString(10)
+    const nanoAmount = ton.utils.toNano(val.toString()).toString(10)
 
     const addressRepository = new AddressRepository()
-    await addressRepository.updateMinAmount(addressId, Number(nanoAmount))
+    await addressRepository.updateMinAmount(addressId, nanoAmount)
 
     const { _id, notifications } = await addressRepository.getOneById(addressId)
+
+    ctx.scene.leave()
 
     return ctx.replyWithHTML(
       ctx.i18n.t('address.notifications.menu'),
@@ -34,7 +35,6 @@ module.exports = async (ctx) => {
         .markup(getAddressNotificationsKeyboard({ _id, notifications }, ctx.i18n)),
     )
   } catch (error) {
-    console.log(error)
     return ctx.replyWithHTML(
       ctx.i18n.t('address.notifications.invalid'),
       Extra.HTML().webPreview(false).markup(getBackToNotificationsKeyboard(addressId, ctx.i18n)),
